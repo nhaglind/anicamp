ddec4ed6455abb336fcf18556cb3f3a4a1ce369f
Nick Haglind <nhaglind@gmail.com>
2017-11-04 11:07:35 -0500

    add controller for stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'project/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'statistics/', to: 'statistics#index'
+  root 'project#index'
+
 end
